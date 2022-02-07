35ba2b2f10a72e8a0fba91cc31bfb262c96a370b
Weilin Luo <weilin.william.luo@gmail.com>
2022-02-07 11:48:15 -0800

    chap1 - fmin()

--- chap1_numerics_math/basic_functions.cpp
+++ chap1_numerics_math/basic_functions.cpp
@@ -9,16 +9,13 @@ The <cmath> header defines an extensive collection of common math functions in t
 #include <cmath>
 
 /*
-Basic Functions
-
 abs(x), fabs(x), fabsf(x), fabsl(x)
 Returns the absolute value of x. abs() and fabs() accept all numeric types.
 
 abs(x), labs(x), llabs(x)
 Defined by <cstdlib>. Return absolute value for an integral x.
 */
-
-int main()
+int main_abs()
 {
     std::cout << "abs ((float)-10.6) = " << std::abs ((float)-10.6) << std::endl;
     std::cout << "abs ((double)-10.6) = " << std::abs ((double)-10.6) << std::endl;
@@ -30,5 +27,34 @@ int main()
 
     return 0;
 }
+
+
+/*
+fmin(x, y)
+fmax(x, y)
+
+Return the minimum or maximum of x and y.
+std::min() and std::max() defined in <algorithm> are often more convenient, as they do not convert integers into double. These are explained later in this chapter.
+*/
+int main_fmin ()
+{
+    std::cout << "fmin ((float)-100.0, (float)1.0) = " << std::fmin ((float)-100.0, (float)1.0) << std::endl;
+    std::cout << "fmin ((double)-100.0, (double)1.0) = " << std::fmin ((double)-100.0, (double)1.0) << std::endl;
+    std::cout << "fmin ((long double)-100.0, (long double)1.0) = " << std::fmin ((long double)-100.0, (long double)1.0) << std::endl;
+
+    std::cout << "fmin ((int)-10, (int)1) = " << std::fmin ((int)-10, (int)1) << std::endl;
+    std::cout << "fmin ((long)-10, (long)1) = " << std::fmin ((long)-10, (long)1) << std::endl;
+    std::cout << "fmin ((long long)-10, (long long)1) = " << std::fmin ((long long)-10, (long long)1) << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
 
 
