4c47d8cee681bf39fac3dc245595c3b44feeb409
Weilin Luo <weilin.william.luo@gmail.com>
2022-02-11 18:32:55 -0800

    consexpr examples

---
+++ chap1_numerics_math/constexpr.cpp
@@ -0,0 +1,75 @@
+/*
+constexpr.cpp
+
+An example showing how to work with constexpr variables, functions, and a user-defined type. 
+
+Note: compile with: cl /EHsc /W4 constexpr.cpp
+*/
+#include <iostream>
+
+using namespace std;
+
+// Pass by value
+constexpr float exp(float x, int n)
+{
+    return n == 0 ? 1 :
+        n % 2 == 0 ? exp(x * x, n / 2) :
+        exp(x * x, (n - 1) / 2) * x;
+}
+
+// Pass by reference
+constexpr float exp2(const float& x, const int& n)
+{
+    return n == 0 ? 1 :
+        n % 2 == 0 ? exp2(x * x, n / 2) :
+        exp2(x * x, (n - 1) / 2) * x;
+}
+
+// Compile-time computation of array length
+template<typename T, int N>
+constexpr int length(const T(&)[N])
+{
+    return N;
+}
+
+// Recursive constexpr function
+constexpr int fac(int n)
+{
+    return n == 1 ? 1 : n * fac(n - 1);
+}
+
+// User-defined type
+class Foo
+{
+public:
+    constexpr explicit Foo(int i) : _i(i) {}
+    constexpr int GetValue() const
+    {
+        return _i;
+    }
+private:
+    int _i;
+};
+
+
+int main()
+{
+    constexpr Foo foo(5);
+
+    // Compile time:
+    constexpr float x = exp(5, 3);
+    constexpr float y { exp(2, 5) };
+    constexpr int val = foo.GetValue();
+    constexpr int f5 = fac(5);
+    const int nums[] { 1, 2, 3, 4 };
+    const int nums2[length(nums) * 2] { 1, 2, 3, 4, 5, 6, 7, 8 };
+    cout << "val = " << val << endl;
+
+    // Run time:
+    const float z = exp(2, 5);
+    cout << "The value of foo is " << foo.GetValue() << endl;
+
+    return 0;
+}
+
+
